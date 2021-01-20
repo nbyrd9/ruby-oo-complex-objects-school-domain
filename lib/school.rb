2f6a867c268c5223ef2f324c21fe3193cfdcfc00
nbyrd9 <nianibyrd@gmail.com>
2021-01-20 09:26:20 -0500

    commit tree a9b258c0e8b2ac1ea85b386456414cb4cb55a28e

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+    attr_accessor :roster, :grade
+    attr_reader :school
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student (name, grade)
+        if @roster[grade] != nil
+            @roster[grade] << name
+       else
+        @roster[grade] = [name]
+       end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |key, value|
+            value.sort!
+        end
+    end
+end
+
+# school = School.new("Bayside High School")
+# # school.add_student("Zach Morris", 9)
+# # school.roster
